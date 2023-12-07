b28e2b3e64a2a5782b0131fd0b876f7910f5a507
Bogdan <bogdanrazvan00@yahoo.com>
2023-12-07 10:47:23 +0200

    fix: 🐛 yarn lint changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { Piece, generatePieceSet } from "@panda-chess/pdc-core"
-import { useEffect, useState } from "react"
+import { Piece, generatePieceSet } from "@panda-chess/pdc-core";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [pieces, setPieces ] = useState<Piece[]>();
+    const [pieces, setPieces ] = useState<Piece[]>();
 
-  useEffect(() => {
-    setPieces(generatePieceSet());
-  }, []);
+    useEffect(() => {
+        setPieces(generatePieceSet());
+    }, []);
 
-  return (
-    <>
-      <p>test</p>
-      {pieces && pieces.map((piece, idx) => {
-        return <p key={idx}>{piece.color} {piece.pieceType} - {piece.position.x} {piece.position.y}</p>
-      })}
-    </>
-  )
+    return (
+        <>
+            <p>test</p>
+            {pieces && pieces.map((piece, idx) => {
+                return <p key={idx}>{piece.color} {piece.pieceType} - {piece.position.x} {piece.position.y}</p>;
+            })}
+        </>
+    );
 }
 
-export default App
+export default App;
