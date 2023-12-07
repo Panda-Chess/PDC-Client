4df4d1b5326d64dee08e0ce0e5cfe5f3ea684b70
Bogdan <bogdanrazvan00@yahoo.com>
2023-12-07 12:25:25 +0200

    fix: 🐛 menu fix

--- src/pages/menu/play.tsx
+++ src/pages/menu/play.tsx
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom";
+import { GameButton } from "../../models/gameButton";
+import { GameTypes } from "../../models/gameTypes";
 
 export const Play = () => {
+    const buttons: GameButton[] = [
+        { label: "Vs AI", gameType: GameTypes.AI },
+        { label: "Casual Offline", gameType: GameTypes.CASUAL_OFFLINE },
+        { label: "Casual Online", gameType: GameTypes.CASUAL_ONLINE },
+        { label: "Competitive", gameType: GameTypes.COMPETITIVE },
+    ];
+
+
     return (
         <div>
-            <h1>Play</h1>
+            <h1>Top Players</h1>
+            {buttons.map((button, idx) => {
+                return (
+                    <div key={idx}>
+                        <Link to={`/game/${button.gameType}`}>{button.label}</Link>
+                    </div>
+                );
+            
+            })}
             <Link to="/">Back</Link>
         </div>
     );
--- src/pages/menu/topPlayers.tsx
+++ src/pages/menu/topPlayers.tsx
@@ -1,27 +1,9 @@
 import { Link } from "react-router-dom";
-import { GameTypes } from "../../models/gameTypes";
-import { GameButton } from "../../models/gameButton";
 
 export const TopPlayers = () => {
-    const buttons: GameButton[] = [
-        { label: "Vs AI", gameType: GameTypes.AI },
-        { label: "Casual Offline", gameType: GameTypes.CASUAL_OFFLINE },
-        { label: "Casual Online", gameType: GameTypes.CASUAL_ONLINE },
-        { label: "Competitive", gameType: GameTypes.COMPETITIVE },
-    ];
-
-
     return (
         <div>
             <h1>Top Players</h1>
-            {buttons.map((button, idx) => {
-                return (
-                    <div key={idx}>
-                        <Link to={`/game/${button.gameType}`}>{button.label}</Link>
-                    </div>
-                );
-            
-            })}
             <Link to="/">Back</Link>
         </div>
     );
